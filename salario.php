f5fecf4c29992f955749f13898c231cd7ffc2ec4
maarialara6 <eduardalara5921@gmail.com>
2026-03-17 22:16:38 -0300

    projeto salario importo

--- salario.php
+++ salario.php
@@ -7,14 +7,20 @@
 
 	echo "Salário liquído: <br>";
 
-	$salB,
+	$salB;
 	$sal;
-	$porc;
+	$porc1=7.5;
+	$porc2=15;
+	$porc3=22.5;
+	$porc4=27.5;
 
 	if ($salB >= && <= 2428.80) {
 		echo "Isento";
-	} elseif ($salB) {
-		// code...
+	} elseif ($salB 2428.81 >= && $salB <=2826.65) {
+		$sal = $salB ($salB - $porc1 / 100);
+		echo "Salario Bruto: $salB<br>";
+		echo "Porcentagem de deconto: $porc1%";
+		echo "Salário liquído: $sal";
 	}
 
 
