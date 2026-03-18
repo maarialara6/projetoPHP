d7cba4e6d689f2aa140dead92a077c9cc6146867
maarialara6 <eduardalara5921@gmail.com>
2026-03-17 21:54:52 -0300

    projeto imposto

---
+++ calculadora.php
@@ -0,0 +1,28 @@
+<?php 
+	$num1 =10;
+	$num2 =20;
+	$op=5;
+
+	switch ($op) {
+		case 1: $resp = $num1 + $num2;
+			$nomeOP= "soma";
+			break;
+
+		case 2: $resp = $num1 - $num2;
+			$nomeOP= "subtração";
+			break;
+
+		case 3: $resp = $num1 / $num2;
+			$nomeOP= "divisão";
+			break;
+
+		case 4: $resp = $num1 * $num2;
+			$nomeOP= "multiplicação";
+			break;
+		
+		default: 
+			echo("Sair");
+			break;
+	}
+	echo "A $nomeOP do $num1 e $num2 é $resp";
+ ?>
---
+++ salario.php
@@ -0,0 +1,17 @@
+<?php
+	echo "Salário Bruto <br>";
+	echo "Imposto de Renda <br>";
+
+	echo "Porcentagem de deconto: % <br>";
+	echo "Valor do desconto  <br>";
+
+	echo "Salário liquído: <br>";
+
+	$valor;
+	$sal;
+	$porc;
+
+	
+
+
+ ?>
