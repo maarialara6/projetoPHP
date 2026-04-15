d333593819367407d852756440fc9a01490992cf
maarialara6 <eduardalara5921@gmail.com>
2026-04-14 22:09:39 -0300

    14_04

--- soma_valores_vetores.php
+++ soma_valores_vetores.php
@@ -3,17 +3,12 @@
 	$num2 = array (50, 60, 70, 80);
 
 	for ($i=0; $i < count($num1) ; $i++) { 
-		$valor[$i] = $num1[$i] + $num2[$i];
-	}
+		$valor[$i] =  $valor[$i] + $num1[$i];
+	};
 
 
-	for($i=0; $i < sizeof($num2); $i++){
+	for($i=0; $i < sizeof($num1); $i++){
 		echo $valor[$i] . "<br>";
 	};
 
-	echo "<br>";
-
-	var_dump($num1);
-	echo"<br>";
-	var_dump($num2);
  ?>
