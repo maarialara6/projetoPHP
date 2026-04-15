7a38ad12e915f583986e4757019a405cdb8fbac9
maarialara6 <eduardalara5921@gmail.com>
2026-04-14 21:32:55 -0300

    14_04

--- array_soma_vetores.php
+++ array_soma_vetores.php
@@ -2,7 +2,17 @@
 	$num1 = array (10, 20, 30, 40);
 	$num2 = array (50, 60, 70, 80);
 
-	
+	for ($i=0; $i < count($num1) ; $i++) { 
+		$valor[$i] = $num1[$i] + $num2[$i];
+	}
+
+
+	for($i=0; $i < 4; $i++){
+		echo $valor[$i] . "<br>";
+	};
+
+	echo "<br>";
+
 
 	var_dump($num1);
 	echo"<br>";
