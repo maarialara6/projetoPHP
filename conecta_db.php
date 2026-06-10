600c49fb6e3cab9a86d0e136e62436d29bc79d94
maarialara6 <eduardalara5921@gmail.com>
2026-06-09 21:26:08 -0300

    first commit

--- conecta_db.php
+++ conecta_db.php
@@ -9,6 +9,12 @@
 	if (mysqli_connect_errno()) {
 		echo "Erro ao conectar a base de dados: " . mysqli_connect_errno();
 	} else{
-		echo "Banco de dados conectado com sucesso!";
-	}
+		//echo "Banco de dados conectado com sucesso!";
+
+		$sql = "insert into tbPessoa(nome, email) values('Etecia', 'sac@etecia.com')";
+
+		mysqli_query($con,$sql);
+
+		mysqli_close($con);
+	} 
  ?>
---
+++ inserindo_dados.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Inserindo Dados PHP</title>
+</head>
+<body>
+
+	<form action="cadastro.php" method="post">
+		Nome: 
+		<input type="text" name="nome">
+		<br>
+		<br>
+		Email:
+		<input type="email" name="email">
+		<br>
+		<br>
+		<input type="submit" name="enviar">
+	</form>
+
+</body>
+</html>
